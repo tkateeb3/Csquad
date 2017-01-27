991f7f72ac64f8efb94d3b8d09dc75e73dff8c87
tkateeb3 <talib.tk@hotmail.com>
2017-01-27 16:13:56 -0500

    Person 5

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -4,22 +4,22 @@ package edu.gatech.oad.antlab.person;
  *  A simple class for person 5
  *  returns their name and a
  *  modified string 
- *  
+ *
  *  @author Bob
  *  @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
+	/** Talib Kateeb */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person5(String pname) {
-    name = pname;
-  }
-  	/**
+	public Person5(String pname) {
+		name = pname;
+	}
+	/**
 	 * This method should take the string
 	 * input and return its characters rotated
 	 * 2 positions.
@@ -30,10 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String newString = input.substring(2, input.length() - 1) + input.charAt(0) + input.charAt(1);
+		return newString;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
@@ -43,7 +43,7 @@ public class Person5 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 }
